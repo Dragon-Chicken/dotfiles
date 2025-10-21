1282a135d5ec3428b9d578d29ca46675c125d692
Dragon-Chicken <ethandsouza256@gmail.com>
2025-10-21 19:01:50 +0530

    readme change and prob some other things

--- README.md
+++ README.md
@@ -25,7 +25,20 @@ git clone https://github.com/Dragon-Chicken/dotfiles.git
 ```sh
 make # or `make install`
 ```
-
+5. I use custom names for workspaces. So, to get the same names edit `~/sxwm/src/defs.h` (or whereever your sxwm source files are at) and replace lines 35 to 44 with this:\
+(workspaces 6 to 9 still exist but there are no keybinds to access them)
+```c
+#define WORKSPACE_NAMES	\
+	"dev""\0"\
+	"web""\0"\
+	"mus""\0"\
+	"ex1""\0"\
+	"ex2""\0"\
+	"6""\0"\
+	"7""\0"\
+	"8""\0"\
+	"9""\0"
+```
 # Uninstall
 To uninstall, simply run:
 ```sh
--- st/st-terminal/config.h
+++ st/st-terminal/config.h
@@ -222,25 +222,26 @@ char *xdndescchar = " !\"#$&'()*;<>?[\\]^`{|}~";
 // rose-pine //
 ///////////////
 static const char *colorname[] = {
-    "#26233a",
-    "#eb6f92",
-    "#9ccfd8",
-    "#f6c177",
-    "#31748f",
-    "#c4a7e7",
-    "#ebbcba",
-    "#e0def4",
-    "#6e6a86",
-    "#eb6f92",
-    "#9ccfd8",
-    "#f6c177",
-    "#31748f",
-    "#c4a7e7",
-    "#ebbcba",
-    "#e0def4",
-    [255] = 0,
-    /* more colors can be added after 255 to use with DefaultXX */
-    [256] = "#191724",
+  "#26233a",
+  "#eb6f92",
+  "#9ccfd8",
+  "#f6c177",
+  "#31748f",
+  "#c4a7e7",
+  "#ebbcba",
+  "#e0def4",
+
+  "#6e6a86",
+  "#eb6f92",
+  "#9ccfd8",
+  "#f6c177",
+  "#31748f",
+  "#c4a7e7",
+  "#ebbcba",
+  "#e0def4",
+  [255] = 0,
+  /* more colors can be added after 255 to use with DefaultXX */
+  [256] = "#191724",
 };
 /*
  * Default colors (colorname index)

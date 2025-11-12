bc1eb801797050387c0401ecb4f24e14887c2af9
Dragon-Chicken <ethandsouza256@gmail.com>
2025-11-12 14:52:00 +0530

    moved to git from rsync for note syncing

--- config/.config/notes/syncing.sh
+++ config/.config/notes/syncing.sh
@@ -1,10 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-locdir="/home/ethan"
-extdir="~/storage/shared/Documents"
+pcdir="/home/ethan/notes"
+tabdir="~/storage/shared/Documents/notes"
 
-# pc to tab
-rsync -rPavhu "$locdir/notes" "mytab:$extdir"
+# pc add and commit changes
+echo "----------
+pc commit:"
+cd $pcdir
+git add .
+git commit -m "pc added and committed from script syncing.sh"
 
-# tab to pc
-rsync -rPavhu "mytab:$extdir/notes" "$locdir"
+echo "-----------
+tab commit:"
+ssh mytab "cd $tabdir; git add .; git commit -m 'tab added and commited from script syncing.sh'"
+
+# pull changes from tab
+echo "--------
+pc pull:"
+cd $pcdir
+git pull mytab:$tabdir
+
+# pull changes from pc
+echo "---------
+tab pull:"
+ssh mytab "cd $tabdir; git pull"
